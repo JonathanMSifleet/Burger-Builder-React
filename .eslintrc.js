fae6267756bf4033ddebdf67b864efe793def470
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2021-03-05 16:03:13 +0000

    Initialise project

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  // prettier-ignore
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'eslint-config-prettier'
+  ],
+  plugins: ['react', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/no-unescaped-entities': 0,
+    'react/jsx-first-prop-new-line': [2, 'multiline'],
+    'react/jsx-max-props-per-line': [2, { maximum: 1, when: 'multiline' }],
+    'react/jsx-indent-props': [2, 2],
+    'react/jsx-closing-bracket-location': [2, 'tag-aligned'],
+    '@typescript-eslint/ban-ts-comment': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
+  }
+};
