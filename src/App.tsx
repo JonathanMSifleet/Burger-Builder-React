99b8ad0234645c36ff89db42128d2634e8c3bdda
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2021-03-23 10:16:27 +0000

    Set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // @ts-ignore
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Layout from './hoc/Layout/Layout';
@@ -8,9 +9,10 @@ class App extends Component {
   render(): JSX.Element {
     return (
       <div>
+        {/* could use switch instead of exact */}
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/" exact component={BurgerBuilder} />
         </Layout>
       </div>
     );
