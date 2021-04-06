fa5eba88a11fbbef0d8e22620a37552c0a9027f5
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2021-04-06 14:05:24 +0100

    Sign up using Firebase auth

--- src/store/actions/auth.ts
+++ src/store/actions/auth.ts
@@ -1,6 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import axios from 'axios';
 import * as actionTypes from './actionTypes';
+
+export interface IAuthData {
+  returnSecureToken: boolean;
+  tenantId: string;
+  token: string;
+}
 
 export const authStart = (): { type: string } => {
   return {
@@ -24,7 +31,46 @@ export const authFail = (error: any): { type: string; error: any } => {
 
 // @ts-ignore
 export const auth = (email: string, password: string) => {
-  return (dispatch: any) => {
+  const signupHandler = async (email: string, password: string) => {
+    const apiKey = 'AIzaSyDvjZNwI1H5NsUPGNLRlt4bCameEpvcqVE';
+    let response;
+    try {
+      response = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
+        {
+          email,
+          password,
+          returnSecureToken: true
+        }
+      );
+      response = response.data;
+    } catch (e) {
+      response = e;
+    } finally {
+      return response;
+    }
+  };
+
+  return async (dispatch: any) => {
     dispatch(authStart());
+
+    const token = await signupHandler(email, password);
+    console.log('token:', token);
+
+    // const authData = {
+    //   token: '',
+    //   returnSecureToken: true
+    // };
+    // const userToken = await signupHandler(email, password);
+
+    // try {
+    //   const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${apiKey}`;
+    //   const response = await axios.post(url, authData);
+    //   console.log(response);
+    //   dispatch(authSuccess(response.data));
+    // } catch (e) {
+    //   console.error(e);
+    //   dispatch(authFail(e));
+    // }
   };
 };
